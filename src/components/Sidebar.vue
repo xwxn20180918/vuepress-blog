286490a05c3a74801d0dfa90f6e58ad7e70a2f5d
xiongwen <xiongwen@zhonglian.com>
2025-02-25 16:50:10 +0800

    fix: logo src

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center font-600 font-size-[1.2rem]">
       <!--<img :src="{astroLogo}" alt="暂无图片" />-->
       <img
-        src="src/assets/astro.svg"
+        src="/src/assets/astro.svg"
         width="40"
         height="40"
         alt="暂无图片"
