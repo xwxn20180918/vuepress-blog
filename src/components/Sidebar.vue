507fb6452c477c9a44e6c576510dea9d3c1f185b
xiongwen <xiongwen@zhonglian.com>
2025-02-25 16:55:25 +0800

    fix: logo src

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center font-600 font-size-[1.2rem]">
       <!--<img :src="{astroLogo}" alt="暂无图片" />-->
       <img
-        src="/src/assets/astro.svg"
+        src="/favicon.svg"
         width="40"
         height="40"
         alt="暂无图片"
@@ -23,7 +23,6 @@
 
 <script setup lang="ts">
 import { computed, onMounted, watch } from 'vue';
-
 computed(() => {});
 
 onMounted(() => {});
